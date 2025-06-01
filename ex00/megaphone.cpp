8fba8024be6e29a3ba6eb15577a3d63b81dbb23c
funa <orz.home35@gmail.com>
2025-06-01 17:35:51 +0900

    🛠️ ex00をadd

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+std::string StrToUpper(const std::string& str)
+{
+	std::string result;
+
+	result = str;
+	for (size_t i = 0; i < result.size(); i++)
+	{
+		if (std::islower(result[i]))
+			result[i] = std::toupper(result[i]);
+	}
+	return (result);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string	str = StrToUpper(argv[i]);
+			std::cout << str;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
